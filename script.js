b020be6cbc10bed107250cac7fa32b318aa4c143
Rahul Verma <86731857+rahulverma6zii@users.noreply.github.com>
2024-06-22 15:17:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let element=document.getElementById("number");
+function increment(){
+    element.innerText++;
+}
+function decrement(){
+    element.innerText--;
+}
+function neutral(){
+    element.innerText=0;
+}
